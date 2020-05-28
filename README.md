2f110301ba957ce35a3f0729818bb02206cb6404
dodderjs <contact@dody.hu>
2020-05-28 11:09:20 +0200

    Initial comment for angular app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WeatherReport
+# plants
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('weatherReport app is running!');
+    expect(page.getTitleText()).toEqual('plants app is running!');
   });
 
   afterEach(async () => {
